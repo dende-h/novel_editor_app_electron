94386dd16f1d827871e4ebefb42e0dfa8c306e06
Hiroya Shitogiden <dende.capo@gmail.com>
2023-01-06 13:44:16 +0900

    ファイル削除

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withTM = require('next-transpile-modules')([
-	'@fullcalendar/common',
-	'@fullcalendar/daygrid',
-	'@fullcalendar/react'
-]);
-
-module.exports = withTM({
-	// any other next.js settings here
-});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-// import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
-// import { getSupabase } from "../utils/supabase";
-
 
 const Index = ({ user, note }) => {
 	
@@ -10,21 +7,5 @@ const Index = ({ user, note }) => {
 		</>
 	);
 };
-//supabaseのデータ取得とAuth0の認証
-// export const getServerSideProps = withPageAuthRequired({
-// 	async getServerSideProps({ req, res }) {
-// 		const {
-// 			user: { accessToken }
-// 		} = getSession(req, res);
-
-// 		const supabase = getSupabase(accessToken);
-
-// 		const { data: note } = await supabase.from("note").select("*").order("created_at", { ascending: true });
-
-// 		return {
-// 			props: { note }
-// 		};
-// 	}
-// });
 
 export default Index;
